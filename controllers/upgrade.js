b9a6bb2c8f7b1e21c07688c5f1cb3b6e0d82f4ef
Taylor Young <tdy721@gmail.com>
2014-03-02 12:23:24 -0600

    claim process saves claimed player name to profile

--- controllers/upgrade.js
+++ controllers/upgrade.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var User = require('../models/User');
 var request = require('request');
 var _ = require('underscore');
 
@@ -22,20 +23,20 @@ exports.upgradeForm = function(req, res){
  */
 
 exports.upgradePlayer = function ( req, res ) {
-  req.assert('name', 'Name cannot be blank').notEmpty();
+  req.assert('player', 'Character name cannot be blank').notEmpty();
   var errors = req.validationErrors();
   if (errors) {
     req.flash('errors', errors);
     return res.redirect( url );
   }
-  var name = req.body.name;
-  var player_json = STARRYBOUND_PLAYERS_DIR + name.toLowerCase() + '.json';
+  var player = req.body.player;
+  var player_json = STARRYBOUND_PLAYERS_DIR + player.toLowerCase() + '.json';
   fs.exists( player_json, function (exists) {
     if ( exists ) {
       request( SERVER_STATUS_ENDPOINT, function (error, response, body) {
         if (!error && response.statusCode == 200) {
           data = JSON.parse( body );
-          if ( _.contains( data.playersOnline, name ) ) {
+          if ( _.contains( data.playersOnline, player ) ) {
             req.flash('info', { msg: 'You need to logout before we can promote you.' });
             return res.redirect( url );
           } else {
@@ -46,8 +47,16 @@ exports.upgradePlayer = function ( req, res ) {
               fs.writeFile( player_json, JSON.stringify(player, null, 4) , function(err) {
                 if(err) { console.log(err); return; } 
                 else {
-                  req.flash('info', { msg: 'You\'re all set! Login and have fun!' });
-                  return res.redirect( url );
+                  // Save the character name to the users profile.
+                  User.findById(req.user.id, function(err, user) {
+                    if (err) return next(err);
+                    user.profile.player = req.body.player;
+                    user.save(function(err) {
+                      if (err) return next(err);
+                      req.flash('info', { msg: 'Character claimed! You\'re all set! Login and have fun!' });
+                      res.redirect('/account');
+                    });
+                  });
                 }
               }); 
             });
